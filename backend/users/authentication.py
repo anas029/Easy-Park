43331431290debf90668960cfc054b0079072968
anas029 <aliyaqoob+github@gmail.com>
2024-01-28 20:35:30 +0300

    Custom Token Authentication

--- backend/users/authentication.py
+++ backend/users/authentication.py
@@ -4,4 +4,8 @@ from rest_framework.authentication import TokenAuthentication
 class CustomTokenAuthentication(TokenAuthentication):
     def authenticate(self, request):
         token = request.COOKIES.get('token')
-        return super().authenticate(request)
+
+        if not token:
+            return None
+
+        return self.authenticate_credentials(token)
