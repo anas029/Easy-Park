8e86e824fae9b141d95d265676e1d02c21d4ecd1
anas029 <aliyaqoob+github@gmail.com>
2024-01-28 22:12:21 +0300

    add customer user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,18 @@
-from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class UserAccount(AbstractUser, PermissionsMixin):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    mobile_number = models.CharField(
+        _('mobile number'), max_length=15, blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
