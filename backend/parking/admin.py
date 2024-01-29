fd28cc3babbbb0ac1006c37b7946f65f4fff0d33
anas029 <aliyaqoob+github@gmail.com>
2024-01-29 18:22:36 +0300

    register models in admin

--- backend/parking/admin.py
+++ backend/parking/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Location, ParkingSpace, Payment, PriceRate, Receipt,
+                     Reservation, Size)
+
+admin.site.register([Location, ParkingSpace, Payment,
+                    PriceRate, Receipt, Reservation, Size])
